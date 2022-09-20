ca90662a3f89fc0c89d0542c1e790572e59e67c8
Huey Tieu <huey.tieu@mrjaffesclass.com>
2022-09-20 11:08:43 -0700

    did strings yahoo lets goo~git add .git add .!

---
+++ Strings.java
@@ -0,0 +1,12 @@
+
+
+public class Strings
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String string1 = "I am a string";
+        String string2 = string1;
+        String string3 = "I am also a string";
+        boolean stringsEqual = string1.equals(string2);
+    }
+}
